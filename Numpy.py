7101a2f965630a7d27359a0adb169220c0944bf9
rajshri-11 <149686814+rajshri-11@users.noreply.github.com>
2024-01-12 14:29:36 +0530

    Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+
+# Create a NumPy array from a Python list
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array(my_list)
+
+# Print the NumPy array
+print("NumPy array:")
+print(my_array)
